a3b3a59faeddb192d0dc58c130db59c6cf1c953d
Michelle Beshears <meeshysu@gmail.com>
2018-09-29 11:32:13 -0500

    yay

--- javascripts/helpers/maths.js
+++ javascripts/helpers/maths.js
@@ -2,4 +2,4 @@ const multiply = (n1, n2) => {
     return n1 * n2;
 };
 
-export {multiply}
+export {multiply};
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,6 @@
-import {printToDom} from './helpers/util.js';
-import {multiply} from './helpers/maths.js';
+import {calculate} from './components/calculator.js';
 
-const calculate = (num1, num2, mathType) => {
-    let answer = 0;
-    switch(mathType){
-        case 'multiply':
-        answer = multiply(num1, num2);
-            break;
-        case 'divide':
-        answer = divide(num1, num2);
-            break;
-        case 'add':
-        answer = add(num1, num2);
-            break;
-        case 'subtract':
-        answer = subtract(num1, num2);
-            break;
-        default:
-            'nope';
-    }
- printToDom(answer, 'result');
-};
+
 calculate (4, 2, 'multiply');
 
 
