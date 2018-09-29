e619d2bfa27aeeaada96a59a33e70ea4da741e8b
Michelle Beshears <meeshysu@gmail.com>
2018-09-29 12:35:13 -0500

    not sure what i just did but i did it really well

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,17 @@
 import {calculate} from './components/calculator.js';
-
-
-// calculate (4, 2, 'multiply');
+import {buttonEvents} from './helpers/buttonEvents.js';
 
 calculate (4, 2, 'subtract');
+
+const initializeApp = () => {
+    buttonEvents();
+}
+initializeApp();
+
+
 
 
 
 
 //this is called a switch statement above which is clearer than writing if and else statements like crazyz. breaks apply to make sure that it's not continuously going thru the switch.
+// calculate (4, 2, 'multiply');
