1b18bc93b5a0bc16db9e585978014bf4f70cae7b
Michelle Beshears <meeshysu@gmail.com>
2018-09-29 14:11:21 -0500

    actually got the button working

--- javascripts/helpers/buttonEvents.js
+++ javascripts/helpers/buttonEvents.js
@@ -1,19 +1,27 @@
-import {addNumber} from '../components/calculator.js'
+import {addNumber, setMathType, calculate} from '../components/calculator.js'
 
-// object
-const calc = {
-    firstNumber: '',
-    secondNumber: '',
-    mathType: '',
-    display: '', //inner html is going to return a string, we want a space in between the paranthensis. 
-}
+
 
 const sevenButton = document.getElementById('sevenButton');
 const eightButton = document.getElementById('eightButton');
+const nineButton = document.getElementById('nineButton');
+const divideButton = document.getElementById('divideButton');
+const equalsButton = document.getElementById('equalButton');
 
 const addNumberCaller = (e) => {
     addNumber(e.target.innerHTML);
 };
+
+const divideButtonEvent = () => {
+    divideButton.addEventListener('click', () => {
+        setMathType('divide');
+    })
+};
+
+const equalsButtonEvent = () => {
+    equalsButton.addEventListener('click', calculate)
+};
+
 
 const sevenButtonEvent = ( ) => {
     sevenButton.addEventListener('click', addNumberCaller)
@@ -23,14 +31,19 @@ const eightButtonEvent = ( ) => {
     eightButton.addEventListener('click', addNumberCaller)
 };
 
-// const nineButtonEvent = ( ) => {
-//     nineButton.addEventListener('click', addNumberCaller)
-// };
+const nineButtonEvent = ( ) => {
+    nineButton.addEventListener('click', addNumberCaller)
+};
+
+
 
 const attachEvents = () => {
     sevenButtonEvent();
     eightButtonEvent();
-    // nineButtonEvent();
+    nineButtonEvent();
+    divideButtonEvent();
+    equalsButtonEvent();
 }
 
 export default attachEvents;
+
