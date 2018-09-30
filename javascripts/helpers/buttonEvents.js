fed32de95cbce929445dc7d8a2950062f146b212
Michelle Beshears <meeshysu@gmail.com>
2018-09-29 19:53:42 -0500

    tis working now

--- javascripts/helpers/buttonEvents.js
+++ javascripts/helpers/buttonEvents.js
@@ -7,6 +7,17 @@ const eightButton = document.getElementById('eightButton');
 const nineButton = document.getElementById('nineButton');
 const divideButton = document.getElementById('divideButton');
 const equalsButton = document.getElementById('equalButton');
+const fourButton = document.getElementById('fourButton');
+const fiveButton = document.getElementById('fiveButton');
+const sixButton = document.getElementById('sixButton');
+const multiplyButton = document.getElementById('multiplyButton');
+const oneButton = document.getElementById('oneButton');
+const twoButton = document.getElementById('twoButton');
+const threeButton = document.getElementById('threeButton');
+const subtractButton = document.getElementById('subtractButton');
+const addButton = document.getElementById('addButton');
+
+
 
 const addNumberCaller = (e) => {
     addNumber(e.target.innerHTML);
@@ -15,6 +26,24 @@ const addNumberCaller = (e) => {
 const divideButtonEvent = () => {
     divideButton.addEventListener('click', () => {
         setMathType('divide');
+    })
+};
+
+const multiplyButtonEvent = () => {
+    multiplyButton.addEventListener('click', () => {
+        setMathType('multiply');
+    })
+};
+
+const subtractButtonEvent = () => {
+    subtractButton.addEventListener('click', () => {
+        setMathType('subtract');
+    })
+};
+
+const plusButtonEvent = () => {
+    plusButton.addEventListener('click', () => {
+        setMathType('add');
     })
 };
 
@@ -35,6 +64,30 @@ const nineButtonEvent = ( ) => {
     nineButton.addEventListener('click', addNumberCaller)
 };
 
+const fiveButtonEvent = ( ) => {
+    fiveButton.addEventListener('click', addNumberCaller)
+};
+
+const sixButtonEvent = ( ) => {
+    sixButton.addEventListener('click', addNumberCaller)
+};
+
+const fourButtonEvent = ( ) => {
+    fourButton.addEventListener('click', addNumberCaller)
+};
+
+const oneButtonEvent = ( ) => {
+    oneButton.addEventListener('click', addNumberCaller)
+};
+
+const twoButtonEvent = ( ) => {
+    twoButton.addEventListener('click', addNumberCaller)
+};
+
+const threeButtonEvent = ( ) => {
+    threeButton.addEventListener('click', addNumberCaller)
+};
+
 
 
 const attachEvents = () => {
@@ -43,6 +96,15 @@ const attachEvents = () => {
     nineButtonEvent();
     divideButtonEvent();
     equalsButtonEvent();
+    fourButtonEvent();
+    fiveButtonEvent();
+    sixButtonEvent();
+    oneButtonEvent();
+    twoButtonEvent();
+    threeButtonEvent();
+    multiplyButtonEvent();
+    subtractButtonEvent();
+    plusButtonEvent();
 }
 
 export default attachEvents;
